2369de5c922e8990b7c4c9cbff9f4e0b4a7ae7a1
Gare_TH <329288574@qq.com>
2021-09-20 00:15:01 -0400

    upload frontend starter files.

---
+++ frontend/checkoutcalculator/src/views/Home.ts
@@ -0,0 +1,6 @@
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class Home extends Vue {}
---
+++ frontend/checkoutcalculator/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>111111</div>
+</template>
+
+<script lang="ts" src="./Home.ts" />
+
+<style lang="scss">
+</style>
