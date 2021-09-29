21b1099c65ddbc47aee637f6e8c5483755479234
GareArc <52963600+GareArc@users.noreply.github.com>
2021-09-29 14:29:47 -0400

    Upload Data.md and API.md (#3)

--- frontend/checkoutcalculator/src/model/Item.ts
+++ frontend/checkoutcalculator/src/model/Item.ts
@@ -1,5 +1,11 @@
 export interface Item{
   name: string,
   icon: string,
+<<<<<<< Updated upstream
   quantity: number
+=======
+  isTaxed: boolean,
+  quantity: number,
+  discount: number
+>>>>>>> Stashed changes
 }
